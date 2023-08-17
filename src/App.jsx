6402af421a5fc826f66df1c37e8ec3079f3a0f38
lucia-caranfa <lucia.caranfa@contabilium.com>
2023-08-17 15:00:45 -0300

    Bootstrap 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NavBar from './components/Bootstrap'
+
+function App() {
+
+  return (
+    <>
+    <h3>Hola!</h3>
+    <NavBar/>
+    </>
+  )
+}
+
+export default App
